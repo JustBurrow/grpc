bb5fd562986a78c9e30418cafe70d1dbe0ba7d9f
Just Burrow <just.burrow@lul.kr>
2019-05-28 23:22:06 +0900

    ADD : 샘플 gRPC 서버 애플리케이션.
    ```
      .   ____          _            __ _ _
     /\\ / ___'_ __ _ _(_)_ __  __ _ \ \ \ \
    ( ( )\___ | '_ | '_| | '_ \/ _` | \ \ \ \
     \\/  ___)| |_)| | | | | || (_| |  ) ) ) )
      '  |____| .__|_| |_|_| |_\__, | / / / /
     =========|_|==============|___/=/_/_/_/
     :: Spring Boot ::        (v2.1.5.RELEASE)
    
    2019-05-28 23:17:54.251  INFO 18595 --- [           main] k.l.g.s.time.runner.SampleTimeRunner     : Starting SampleTimeRunner on Main-Satation.local with PID 18595 (/Users/justburrow/Repositories/github.com/JustBurrow/grpc/sample-time/out/production/classes started by justburrow in /Users/justburrow/Repositories/github.com/JustBurrow/grpc)
    2019-05-28 23:17:54.253 DEBUG 18595 --- [           main] k.l.g.s.time.runner.SampleTimeRunner     : Running with Spring Boot v2.1.5.RELEASE, Spring v5.1.7.RELEASE
    2019-05-28 23:17:54.254  INFO 18595 --- [           main] k.l.g.s.time.runner.SampleTimeRunner     : No active profile set, falling back to default profiles: default
    2019-05-28 23:17:54.616  INFO 18595 --- [           main] k.l.grpc.sample.time.api.TimeServiceApi  : init complete : kr.lul.grpc.sample.time.api.TimeServiceApi@f9d87b
    2019-05-28 23:17:54.790  INFO 18595 --- [           main] k.l.g.s.time.runner.SampleTimeRunner     : Started SampleTimeRunner in 0.783 seconds (JVM running for 1.281)
    ```

---
+++ sample-time/src/main/java/kr/lul/grpc/sample/time/runner/SampleTimeRunner.java
@@ -0,0 +1,19 @@
+package kr.lul.grpc.sample.time.runner;
+
+import kr.lul.grpc.sample.time.api.TimeServiceApi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author justburrow
+ * @since 2019-05-28
+ */
+@SpringBootApplication(scanBasePackageClasses = TimeServiceApi.class)
+public class SampleTimeRunner {
+  public static void main(String[] args) {
+    SpringApplication application = new SpringApplication(SampleTimeRunner.class);
+    application.setWebApplicationType(WebApplicationType.NONE);
+    application.run(args);
+  }
+}
